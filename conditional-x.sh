9ce495769651da7d62ad6543adb8eeedab6dc3e0
Ubuntu <tanja@tanjuska2.50hbsyhp211epmhmcf0oluibxf.zx.internal.cloudapp.net>
2022-12-16 20:46:27 +0000

    test

--- conditional-x.sh
+++ conditional-x.sh
@@ -4,8 +4,10 @@ message="commit"
 echo "Commit message:"
 read message
 
-git add . && git commit -m $message && git push
+git add . && git commit -m $message
 git commit || echo "Git commit failed"
+
+git push
 
 cs () {
     cd "$@" && ls
